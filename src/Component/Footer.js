644012dfaf485848567f5684e86e37f566137582
DTDUONG98 <dotienduongptit@gmail.com>
2020-05-12 22:42:01 +0700

    update footer

--- src/Component/Footer.js
+++ src/Component/Footer.js
@@ -2,68 +2,103 @@ import React, { Component } from 'react'
 import withStyles from '@material-ui/core/styles/withStyles'
 import { Col, Row } from 'reactstrap'
 import Grid from '@material-ui/core/Grid'
+import GitHubIcon from '@material-ui/icons/GitHub'
+import FacebookIcon from '@material-ui/icons/Facebook'
+import TwitterIcon from '@material-ui/icons/Twitter'
+import Sidebar from '../Component/Sidebar'
 // import Map from './Map'
 const style = theme => ({
-    footer: {
-        background: '#3f51b5',
-        marginTop: '20px',
-    },
-    service: {
-        marginTop: '20px',
-        marginBottom: '10px',
-        cursor: 'pointer'
-    },
-    root: {
-        flexGrow: 1,
-      },
-    grid:{
-        marginLeft: '15px',
-        marginTop: '20px'
-    }
+  footer: {
+    backgroundColor: '#20c997',
+    marginTop: '20px',
+  },
+  service: {
+    marginTop: '30px',
+    marginBottom: '10px',
+    marginLeft: 10,
+    cursor: 'pointer',
+    color: '#f8f9fa',
+    fontFamily: 'auto',
+    fontSize: 20,
+  },
+  root: {
+    flexGrow: 1,
+  },
+  mainGrid: {
+    marginTop: theme.spacing(2),
+    color:'#6c757d'
+  },
 })
+
+const sidebar = {
+  social: [
+    { name: 'GitHub', icon: GitHubIcon },
+    { name: 'Twitter', icon: TwitterIcon },
+    { name: 'Facebook', icon: FacebookIcon },
+  ],
+};
+
+
 class Footer extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-        }
+  constructor(props) {
+    super(props);
+    this.state = {
     }
-    render() {
-        const { classes } = this.props
-        return (
-            <div>
-                <Col md="12" xs="12" className={classes.footer}>
-                    <Grid container className={classes.root} spacing={2} >
-                        <Grid item xs={0} md={3}>
-                            <h4>Hỗ Trợ Khách Hàng</h4>
-                            <h6 style={{color: 'red', marginTop: '20px'}}>Hotline đặt hàng: 0326609183</h6>
-                            <span>(Hỗ trợ từ thứ 2 đến thứ 7)</span>
-                            <h6 style={{color: 'red', marginTop: '10px'}}>Hotline chăm sóc khách hàng: 0326609183</h6>
-                            <p>(Hỗ trợ từ thứ 2 đến thứ 7)</p>
-                            <p><a href="#" className={classes.service}>Các câu hỏi thường gặp</a></p>
-                            <p className={classes.service}><a  href="#" >Gửi yêu cầu hỗ trợ</a></p>
-                            <p className={classes.service}><a  href="#" >Hướng dẫn đặt hàng</a></p>
-                            <p className={classes.service}><a  href="#" >Phương thức vận chuyển</a></p>
-                            <p className={classes.service}><a  href="#" >Chính sách đổi trả</a></p>
-                            <p className={classes.service}><a  href="#" >Chính sách khách hàng</a></p>
-                            <p>Hỗ trợ khách hàng :admin@pro.com</p>
-                            <p>Báo lỗi bảo mật : sercurty@pro.com</p>
-                        </Grid>
-                        <Grid item xs={12} md={3}>
-                            <h4>Về PRO</h4>
-                            <p style={{marginTop: '20px'}} className={classes.service}><a  href="#" >Giới thiệu về PRO</a></p>
-                            <p className={classes.service}><a  href="#" >Chính sách bảo mật thanh toán</a></p>
-                            <p className={classes.service}><a  href="#" >Chính sách bảo mật thông tin cá nhân</a></p>
-                            <p className={classes.service}><a  href="#" >Chính sách giải quyết khiếu nại</a></p>
-                            <p className={classes.service}><a  href="#" >Điều khoản sử dụng</a></p>
-                            <p className={classes.service}><a  href="#">PRO tư vấn</a></p>
-                        </Grid>
-                        <Grid item xs={12} md={6}>
-                            {/* <Map /> */}
-                        </Grid>
-                    </Grid>
-                </Col>
-            </div>
-        )
-    }
+  }
+  Send() {
+    this.props.link.history.push('/Send/123')
+  }
+  Questions() {
+    this.props.link.history.push('/Questions/124')
+  }
+  render() {
+    const { classes } = this.props
+    return (
+      <div>
+        <Col md="12" xs="12" className={classes.footer}>
+          <Grid container className={classes.root} spacing={2} >
+            <Grid item md={2} className={classes.service}>
+              <span style={{color: '#6c757d'}}>BITRIX</span>
+              <p>Bitrix24h</p>
+              <p>Giá cả</p>
+              <p>Giới thiệu</p>
+              <p>Công cụ báo chí</p>
+              <p>Liên hệ với chung tôi</p>
+              <p>Trong báo chí</p>
+            </Grid>
+            <Grid item md={3} className={classes.service}>
+              <span style={{color: '#6c757d'}}>HỖ TRỢ</span>
+              <p>Bộ phận hỗ trợ</p>
+              <p>Hội thảo Web</p>
+              <p>Video hướng dẫn</p>
+              <p>Liên kết với bộ phận hỗ trợ</p>
+              <p>Xếp lịch demo</p>
+              <p>Trang trạng thái Bitrix24</p> 
+            </Grid>
+            <Grid item md={2} className={classes.service}>
+              <span style={{color: '#6c757d'}}>TÀI NGUYÊN</span>
+              <p>Giải pháp</p>
+              <p>Lời khen tặng</p>
+              <p>Thay thế</p>
+              <p>Sử dụng</p>
+            </Grid>
+            <Grid item md={2} className={classes.service}>
+            <span style={{color: '#6c757d'}}>ĐỐI TẮC</span>
+              <p>Tìm đối tác</p>
+              <p>Trở thành đối tác</p>
+              <p>Đăng nhập cho đối tác</p>
+            </Grid>
+            <Grid spacing={2} className={classes.mainGrid}>
+              <Sidebar
+                title={sidebar.title}
+                description={sidebar.description}
+                social={sidebar.social}
+              />
+            </Grid>
+          </Grid>
+        </Col>
+      </div>
+    )
+  }
 }
 export default withStyles(style)(Footer);
