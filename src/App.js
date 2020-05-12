d2e0631b00afb5c5c5388eb98da9a4fc63f070f0
DTDUONG98 <dotienduongptit@gmail.com>
2020-05-12 21:42:31 +0700

    update slider

--- src/App.js
+++ src/App.js
@@ -1,24 +1,28 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Slider from './Component/Slider'
+import Grid from '@material-ui/core/Grid'
+import Button from '@material-ui/core/Button'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Grid container spacing={0}>
+        <Grid item lg={12}>
+          <Grid item lg={2}>
+            <img src="../public/image/logo.png" />
+          </Grid>
+          <Grid item lg={10}>
+            <Button variant="contained" color="primary">
+              Primary
+            </Button>
+          </Grid>
+        </Grid>
+        <Grid item lg={12}>
+          <Slider />
+        </Grid>
+      </Grid>
     </div>
   );
 }
